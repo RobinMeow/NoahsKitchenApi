8794dd8eea224f779123254a9d31798560ec82dc
robinmeow97@gmail.com <111490292+RobinMeow@users.noreply.github.com>
2023-11-23 21:46:18 +0100

    nice test

---
+++ Tests/Auth/ControllerTests.cs
@@ -0,0 +1,47 @@
+using api.Controllers.Auth;
+using api.Domain;
+using api.Infrastructure.Auth;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+
+namespace Auth;
+
+public sealed class ControllerTests
+{
+    readonly AuthController _authController;
+
+    public ControllerTests()
+    {
+        DbContext dbContext = Substitute.For<DbContext>();
+
+        _authController = new AuthController(
+            dbContext,
+            Substitute.For<ILogger<AuthController>>(),
+            Substitute.For<IPasswordHasher>(),
+            Substitute.For<IJwtFactory>()
+        );
+    }
+
+    [Fact]
+    public async Task Register()
+    {
+        var requestDto = new RegisterChefDto()
+        {
+            Name = "Robin",
+            Password = "Password"
+        };
+
+        var expectedCreatedResult = new CreatedResult();
+
+        CreatedResult? meow = await _authController.Register(requestDto) as CreatedResult;
+
+        Equal(expectedCreatedResult.StatusCode, meow?.StatusCode);
+    }
+
+    [Fact]
+    public async Task Register_With_NUll_Throws()
+    {
+        await ThrowsAnyAsync<NullReferenceException>(async () => await _authController.Register(null!));
+    }
+}
